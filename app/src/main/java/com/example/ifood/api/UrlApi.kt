15a7178595981589288469a7206d56dc9ac14503
Lorrana Santos <67704928+lorranasantos@users.noreply.github.com>
2024-05-28 23:19:25 -0300

    refactor: register Address and Restaurant

--- app/src/main/java/com/example/ifood/api/UrlApi.kt
+++ app/src/main/java/com/example/ifood/api/UrlApi.kt
@@ -2,7 +2,7 @@ package com.example.ifood.api
 
 class UrlApi {
     companion object{
-        const val BASE_URL = "http://192.168.100.63:3333/"
+        const val BASE_URL = "http://localhost:3333/"
     }
 
 }
